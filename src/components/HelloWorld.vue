e31b3141bde4677bc6c0391be2306b4acb12f481
Pavel <gpv86x@gmail.com>
2021-10-22 16:25:51 +0300

    lesson #2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,22 +3,66 @@
     <div class="display">
       <input v-model.number="operand1" type="number" />
       <input v-model.number="operand2" type="number" />
-      = {{ result }}
+      = {{ result }} <br />
+      fib(<input v-model.number="operand1" />) fib(<input
+        v-model.number="operand2"
+      />) = {{ fibResult }}
     </div>
+    <div v-show="error">Ошибка! {{ error }}</div>
     <div class="keyboard">
-      <button v-on:click="result = operand1 + operand2">+</button>
-      <button v-on:click="result = operand1 - operand2">-</button>
-      <button @click="result = operand1 / operand2">/</button>
-      <button @click="result = operand1 * operand2">*</button>
-      <button @click="result = operand1 ** operand2">**</button>
-      <button @click="result = (operand1 - (operand1 % operand2)) / operand2">
-        integer didsion
+      <button
+        v-for="operand in operands"
+        v-bind:key="operand"
+        v-bind:title="operand"
+        v-bind:disabled="operand1 === '' || operand2 === ''"
+        @click="calculate(operand)"
+      >
+        {{ operand }}
       </button>
+      <br />
+      <input type="checkbox" id="screenKeyboard" v-model="screenKeyboardShow" />
+      <label for="screenKeyboard">Screen keyboard</label>
+
+      <button
+        v-show="screenKeyboardShow"
+        v-for="key in keyboards"
+        v-bind:key="key"
+        v-bind:title="key"
+        @click="enterNum(key, chosenOperand)"
+      >
+        {{ key }}
+      </button>
+      <br />
+      <input
+        type="radio"
+        id="operand1"
+        value="operand1"
+        v-model="chosenOperand"
+      />
+      <label for="operand1">Operand One</label>
+
+      <input
+        type="radio"
+        id="operand2"
+        value="operand2"
+        v-model="chosenOperand"
+      />
+      <label for="operand2">Operand Two</label>
+      <br />
+    </div>
+    <div class="strange-message">
+      <template v-if="result < 0">Получилось отрицательное число</template>
+      <template v-else-if="result < 100">Результат в первой сотне</template>
+      <template v-else>Получилось слишком большое число</template>
+    </div>
+    <div class="logs">
+      <div v-for="(log, id) in logs" v-bind:key="id">{{ log }}</div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
 export default {
   name: "Calculator",
   data() {
@@ -26,7 +70,101 @@ export default {
       operand1: 0,
       operand2: 0,
       result: 0,
+      error: "",
+      operands: ["+", "-", "/", "*", "**", "integer divsion"],
+      logs: {},
+      screenKeyboardShow: false,
+      chosenOperand: "",
+      keyboards: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
+        "<-Backspace",
+      ],
     };
+  },
+  methods: {
+    add() {
+      this.result = this.operand1 + this.operand2;
+      this.fibResult = this.fibb1 + this.fibb2;
+    },
+    substract() {
+      this.result = this.operand1 - this.operand2;
+      this.fibResult = this.fibb1 - this.fibb2;
+    },
+    divide() {
+      const { operand1, operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "Делить на 0 нельзя!";
+      } else {
+        this.result = operand1 / operand2;
+        this.fibResult = this.fibb1 / this.fibb2;
+      }
+    },
+    multiply() {
+      this.result = this.operand1 * this.operand2;
+      this.fibResult = this.fibb1 * this.fibb2;
+    },
+    pow() {
+      this.result = this.operand1 ** this.operand2;
+      this.fibResult = this.fibb1 ** this.fibb2;
+    },
+    intDivision() {
+      this.result =
+        (this.operand1 - (this.operand1 % this.operand2)) / this.operand2;
+      this.fibResult = this.fibb1 - (this.fibb1 % this.fibb2) / this.fibb2;
+    },
+    calculate(operation = "+") {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.add();
+          break;
+        case "-":
+          this.substract();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "/":
+          this.divide();
+          break;
+        case "**":
+          this.pow();
+          break;
+        case "integer divsion":
+          this.intDivision();
+          break;
+      }
+      const key = Date.now();
+      const value = `${this.operand1}${operation}${this.operand2}=${this.result}`;
+      Vue.set(this.logs, key, value);
+    },
+    fib(n) {
+      return n <= 1 ? n : this.fib(n - 1) + this.fib(n - 2);
+    },
+    enterNum(numb, operand) {
+      if (numb == "<-Backspace") {
+        let str = this[operand].toString();
+        str = str.slice(0, -1);
+        this[operand] = parseInt(str);
+      } else return (this[operand] += numb);
+    },
+  },
+  computed: {
+    fibb1() {
+      return this.fib(this.operand1);
+    },
+    fibb2() {
+      return this.fib(this.operand2);
+    },
   },
 };
 </script>
@@ -46,5 +184,11 @@ li {
 }
 a {
   color: #42b983;
+}
+div {
+  margin: 25px;
+}
+button {
+  margin: 10px 2px 5px 2px;
 }
 </style>
